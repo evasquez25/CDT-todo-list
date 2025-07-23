2e0755ab48db4810bc6b0037586728f3df0e1949
Eric Vasquez <evasquezreyes@college.harvard.edu>
2025-07-22 20:57:20 -0400

    Part 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import TodoList from './TodoList'
+import TodoForm from './TodoForm'
 
 function App() {
 
   return (
     <div>
       <h1>My Todos</h1>
+      <TodoForm></TodoForm>
       <TodoList></TodoList>
     </div>
   )
