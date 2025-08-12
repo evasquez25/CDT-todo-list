e043af7f32566bfd1aa57f0a8006b455172d01e2
Eric Vasquez <evasquezreyes@college.harvard.edu>
2025-08-12 18:42:24 -0400

    fix: part 4 complete. Disabled form submit button when input is empty

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -15,7 +15,7 @@ function TodoForm({ onAddTodo }) {
         <form onSubmit={handleAddTodo}>
             <label htmlFor='todoTitle'>Todo</label>
             <input id='todoTitle' name='title' value={workingTodoTitle} onChange={(event) => setWorkingTodo(event.target.value)} ref={todoTitleInput}></input>
-            <button>Add Todo</button>
+            <button disabled={!workingTodoTitle}>Add Todo</button>
         </form>
     )
 }
