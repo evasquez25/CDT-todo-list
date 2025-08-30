bc0b70adbf66979721e17f079591e85f6e56dfc9
Eric Vasquez <evasquezreyes@college.harvard.edu>
2025-08-30 16:50:35 -0400

    branch setup with small changes from part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import TodoList from './features/TodoList/TodoList'
 import TodoForm from './features/TodoForm'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-
   const [ todoList, setTodoList ] = useState([])
+  const [ isLoading, setIsLoading ] = useState(false)
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`
+  const token = `Bearer ${import.meta.env.VITE_PAT}`
 
   function addTodo(title) {
     const newTodo = {
@@ -38,6 +40,48 @@ function App() {
 
     setTodoList(updatedTodos)
   }
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true)
+
+      const options = {
+        method: "GET",
+        headers: {
+          "Authorization": token
+        }
+      }
+
+      try {
+        const resp = await fetch(url, options)
+        if (!resp.ok) {
+          throw new Error(resp.message)
+        }
+
+        const data = await resp.json()
+
+        const fetchedTodos = data.records.map((record) => {
+          const todo = {
+            // Assign record properties from json to the appropriate todo properties
+            id: record.id,
+            ...record.fileds
+          }
+          if (!todo.isCompleted) {
+            // Airtable doesn't return properties whose values are false or empty strings
+            // Set property to false so field exists and prevents logic bugs
+            todo.isCompleted = false 
+          }
+          return todo
+        })
+        setTodoList([...fetchedTodos])
+      } catch(error) {
+        //TODO
+      } finally {
+        //TODO
+      }
+    }
+    fetchTodos()
+  }, [])
 
   return (
     <div>
