43ca335d88dd1fa9312876ca5a01768e4184b1f7
Eric Vasquez <evasquezreyes@college.harvard.edu>
2025-10-01 19:09:44 -0400

    feat: Part 2 complete, reducer function defined in reducer file

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -17,6 +17,33 @@ const actions = {
     clearError: 'clearError'
 }
 
+function todosReducer(state = initialState, action) {
+    switch(action.type) {
+        case actions.fetchTodos:
+            return {...state,};
+        case actions.loadTodos:
+            return {...state,};
+        case actions.setLoadError:
+            return {...state,};
+        case actions.startRequest:
+            return {...state,};
+        case actions.addTodo:
+            return {...state,};
+        case actions.endRequest:
+            return {...state,};
+        case actions.updateTodo:
+            return {...state,};
+        case actions.completeTodo:
+            return {...state,};
+        case actions.revertTodo:
+            return {...state,};
+        case actions.clearError:
+            return {...state,};
+        default:
+            return state;
+    }
+}
+
 const initialState = {
     todoList: [],
     isLoading: false,
